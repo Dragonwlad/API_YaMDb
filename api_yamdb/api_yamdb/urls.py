8cdcc591b7341d3861846d30c1671bd96bd621aa
Sergey Mazilin <mazilin.sergey@yandex.ru>
2023-07-27 19:04:19 +0400

    Добавил импорт csv файлов

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('import/', views.data_import),
     path('api/', include('api.urls')),
     path(
         'redoc/',
