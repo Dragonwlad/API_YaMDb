900080bf699328048f9f4df0349a59b3a8f9f387
Sergey Mazilin <mazilin.sergey@yandex.ru>
2023-07-29 20:06:20 +0400

    Перенес подсчет рейтинга произведений

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,6 +9,7 @@ from .serializers import (CategorySerializer, GenreSerializer,
                           TitleListSerializer,ReviewSerializer,
                           CommentSerializer,TitleDetailSerializer,
                           TitleManageSerealizer)
+from users.models import User
 from .permissions import IsSuperUserIsAdminIsModeratorIsAuthor
 
 
@@ -24,18 +25,18 @@ class TitleViewSet(viewsets.ModelViewSet):
             return TitleDetailSerializer
         return super().get_serializer_class()
     
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        reviews = Review.objects.filter(title=instance)
-        score_sum = 0
-        if reviews.count() != 0:
-            for review in reviews:
-                score_sum += review.score
-                print(f'{reviews.count()}>>>>>>>>>>>>{score_sum}')
-            instance.rating = round(score_sum / reviews.count(), 2)
-            instance.save()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+    # def retrieve(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     reviews = Review.objects.filter(title=instance)
+    #     score_sum = 0
+    #     if reviews.count() != 0:
+    #         for review in reviews:
+    #             score_sum += review.score
+    #             print(f'{reviews.count()}>>>>>>>>>>>>{score_sum}')
+    #         instance.rating = round(score_sum / reviews.count(), 2)
+    #         instance.save()
+    #     serializer = self.get_serializer(instance)
+    #     return Response(serializer.data)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -53,19 +54,36 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     """Вьюсет Для объектов модели Отзывы"""
     serializer_class=ReviewSerializer
-    permission_classes=(permissions.IsAuthenticatedOrReadOnly,
-                         IsSuperUserIsAdminIsModeratorIsAuthor)
+    # permission_classes=(permissions.IsAuthenticatedOrReadOnly,
+    #                      IsSuperUserIsAdminIsModeratorIsAuthor)
+    permission_classes=(permissions.AllowAny,)
 
     def get_title(self):
         title_id=self.kwargs.get('title_id')
-        return get_object_or_404(Title, pk=title_id)    
+        return get_object_or_404(Title, pk=title_id)
+    
+    def save_actual_score(self):
+        title = self.get_title()
+        reviews = Review.objects.filter(title=title)
+        if reviews.count() != 0:
+            score_sum = 0
+            for review in reviews:
+                score_sum += review.score
+            title.rating = round(score_sum / reviews.count(), 2)
+            title.save()
 
     def get_queryset(self):
         return self.get_title().reviews.all()
     
     def perform_create(self, serializer):
-        
+        self.save_actual_score()
+        author = User.objects.get(id=1)
+        # serializer.save(author=author, title=self.get_title())
         serializer.save(author=self.request.user, title=self.get_title())
+
+    def perform_update(self, serializer):
+        self.save_actual_score()
+        serializer.save()
 
 
 class CommentViewSet(viewsets.ModelViewSet):    
